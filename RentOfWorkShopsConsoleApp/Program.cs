b640be89b89a8c14302873c1ad02963c8c8a57cc
211925 <211925@1C1B0D635E5B1>
2022-05-06 13:19:07 +0300

    Добавьте файлы проекта.

---
+++ RentOfWorkShopsConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RentOfWorkShopsConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
